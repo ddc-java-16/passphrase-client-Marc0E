faad5c30346e74e2cb493588e994e55f68c432ef
Marc0E <mestradamonge@cnm.edu>
2023-11-03 09:33:07 -0600

    Edit passphrase fragment in progress.

---
+++ app/src/main/java/edu/cnm/deepdive/passphrase/controller/EditPassphraseFragment.java
@@ -0,0 +1,9 @@
+package edu.cnm.deepdive.passphrase.controller;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+public class EditPassphraseFragment extends DialogFragment {
+
+
+}
